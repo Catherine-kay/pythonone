e18deb285b98df3773e641a0273696ab4d49f452
Catherine-Kay <1355910204@qq.com>
2020-10-02 16:27:30 +0800

    python常用的方法和数据类型的学习

---
+++ demo01.py
@@ -0,0 +1,27 @@
+"""
+print("你好呀！")
+print(2333)
+print(2.333)
+print(True) #布尔值
+print({})
+print(())
+print("哈哈",2333)
+print(((23+25)*8)/2)
+print(2>3)
+
+a = input("请输入内容：")
+b = input("请输入内容：")
+print("input获取的值:",a+b)
+
+# print(type("你好"))
+# print(type(2333))
+# print(type(2.33))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+a = int(2333)
+print(type(a))
+"""
+a =input("请输入内容：")
+b = input("请输入内容：")
+print("input获取的值:",len(a+b))
